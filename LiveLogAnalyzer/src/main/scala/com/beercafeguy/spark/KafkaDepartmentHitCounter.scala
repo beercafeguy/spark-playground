c34912863cd445b284531df647ae3967915ce7e7
beercafeguy <hemchandra@outlook.com>
2018-09-10 22:40:42 +0530

    adding changes for department hit counter

--- LiveLogAnalyzer/src/main/scala/com/beercafeguy/spark/KafkaDepartmentHitCounter.scala
+++ LiveLogAnalyzer/src/main/scala/com/beercafeguy/spark/KafkaDepartmentHitCounter.scala
@@ -13,28 +13,32 @@ object KafkaDepartmentHitCounter {
     val windowInterval=args(1).toInt
     val topicName=args(2);
     val hdfsLocation=args(3)
+    val bootstrapServers=args(4)
     val topicSet=Set(topicName)
     val kafkaParams = Map[String, String](
-      "bootstrap.servers" -> "kafka1.host.beercafeguy.com:9092,kafka2.host.beercafeguy.com:9092,kafka3.host.beercafeguy.com:9092"
+      "bootstrap.servers" -> bootstrapServers,
+      "zookeeper.connect" -> "or1hdp600.corp.adobe.com:2181,or1hdp601.corp.adobe.com:2181,or1hdp609.corp.adobe.com:2181",
+      "group.id" -> "spark-streaming-test",
+      "zookeeper.connection.timeout.ms" -> "1000"
     )
 
     val conf=new SparkConf().setAppName("KafkaDepartmentHitCounter").setMaster(master)
     val sc=new SparkContext(conf)
     val streamingContext=new StreamingContext(sc,Seconds(windowInterval))
 
-    val logStream = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](streamingContext, kafkaParams, topicSet)
+    val logStream = KafkaUtils.
+      createDirectStream[String, String, StringDecoder, StringDecoder](streamingContext, kafkaParams, topicSet)
     val logs=logStream.map(log => log._2)
-    val departments=logs.filter(message => {
+    logs.saveAsTextFiles(hdfsLocation)
+    /*val departments=logs.filter(message => {
       val m = message.split(" ")
       m(6).startsWith("/department/")
     }).map(dept => (dept.split(" ")(6).split("/")(2),1))
 
     val departmentsCount=departments.reduceByKey(_+_)
-    departmentsCount.saveAsTextFiles(hdfsLocation)
+    departmentsCount.saveAsTextFiles(hdfsLocation)*/
 
     streamingContext.start()
     streamingContext.awaitTermination()
-
-
   }
 }
